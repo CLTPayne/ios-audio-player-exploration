072a001d7e078aee43ab29033a8fc3b431e747fd
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-11-20 23:15:53 +0000

    Initial storyboard for music player controls

---
+++ IOS-audio-player-exploration/IOS-audio-player-exploration/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  IOS-audio-player-exploration
+//
+//  Created by Charlotte Payne on 20/11/2019.
+//  Copyright © 2019 Charlotte Payne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var trackTitle: UILabel!
+    
+    @IBOutlet weak var playedTime: UILabel!
+    
+    @IBAction func playOrPauseMusic(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopMusic(_ sender: Any) {
+    }
+}
+
