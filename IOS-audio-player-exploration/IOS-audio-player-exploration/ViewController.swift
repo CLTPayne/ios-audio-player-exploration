8fc024b122a805576c0331a1c4d8eec517fda7d4
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-11-20 23:31:39 +0000

    Implement play, pause and stop via view controller
    * Ctrl and drag from the UI element to add shell function to the view controller

--- IOS-audio-player-exploration/IOS-audio-player-exploration/ViewController.swift
+++ IOS-audio-player-exploration/IOS-audio-player-exploration/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
+    var isPlaying = false
+    var timer: Timer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        trackTitle.text = "Future Islands - Tin Man"
+        let path = Bundle.main.path(forResource: "Future Islands - Tin Man", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+        } catch {
+            print("Can't load audio 🤯 ", error)
+        }
+    }
+    
+    @objc func updateTime() {
+        let currentTime = Int(audioPlayer.currentTime)
+        let minutes = currentTime / 60
+        let seconds = currentTime - minutes * 60
+        
+        playedTime.text = String(format: "%02d:%02d", minutes, seconds) as String
     }
 
     @IBOutlet weak var trackTitle: UILabel!
@@ -20,10 +43,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var playedTime: UILabel!
     
     @IBAction func playOrPauseMusic(_ sender: Any) {
+        if isPlaying {
+            audioPlayer.pause()
+            isPlaying = false
+        } else {
+            audioPlayer.play()
+            isPlaying = true
+            
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        }
     }
     
     
     @IBAction func stopMusic(_ sender: Any) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0
+        isPlaying = false
     }
 }
 
